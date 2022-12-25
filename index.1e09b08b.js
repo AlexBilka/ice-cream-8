1b24cfc704c0a9958e58042e6b14594805ce19eb
AlexBilka <114955573+AlexBilka@users.noreply.github.com>
2022-12-25 21:06:41 +0000

    Deploying to gh-pages from @ AlexBilka/ice-cream-8@a3c71301289f8a0bebb719c764b8b86e57e1db42 🚀

---
+++ index.1e09b08b.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-read-more-open]"),closeModalBtn:document.querySelector("[data-modal-read-more-close]"),modal:document.querySelector("[data-modal-read-more]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.1e09b08b.js.map
