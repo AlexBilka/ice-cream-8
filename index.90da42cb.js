d4418bd8f0d146045905c3f01e8774f163f82d2f
AlexBilka <114955573+AlexBilka@users.noreply.github.com>
2022-12-26 21:10:14 +0000

    Deploying to gh-pages from @ AlexBilka/ice-cream-8@cdd4911c592cbba19f23020ddb6770006765a205 🚀

--- index.90da42cb.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){new Swiper(".customer-reviews-swiper",{pagination:{el:".customer-reviews-swipes",bulletActiveClass:"active",bulletClass:"customer-reviews-swipepoint",bulletElement:"li",type:"bullets"}})}));
-//# sourceMappingURL=index.90da42cb.js.map
