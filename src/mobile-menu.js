498f396cc8d484371d16e4f09034c301cc8f8f34
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2022-12-27 14:40:20 +0200

    Зміна js для приховування модального вікна

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,25 +1,32 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
+  const mobileButton = document.querySelector('.js-menu-button');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  const noScroll = document.querySelector('body');
+  const bodyScroll = document.querySelector('.js-scroll');
+  const menuClose = document.querySelector('.header__nav-mobile');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-    noScroll.classList.toggle('no-scroll');
+    mobileButton.classList.toggle('is-open');
+    bodyScroll.classList.toggle('no-scroll');
+  };
+
+  const removeMenu = () => {
+    mobileMenu.classList.remove('is-open');
+    mobileButton.classList.remove('is-open');
+    bodyScroll.classList.remove('no-scroll');
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  menuClose.addEventListener('click', removeMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    noScroll.classList.toggle('no-scroll');
+    mobileButton.classList.remove('is-open');
+    bodyScroll.classList.remove('is-open');
   });
 })();
