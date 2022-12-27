84461fd2c42567f817b7d2b053f6ce4fe684f368
Yuliya Maslyanenko <yuliyamas86@gmail.com>
2022-12-27 23:31:43 +0200

    modal-buy-now js

---
+++ src/modal-buy-now.js
@@ -0,0 +1,38 @@
+
+
+    (() => {
+        const refs = {
+            openModalBtn: document.querySelector("[data-modal-buy-now-header-open]"),
+            openModalBtnMob: document.querySelector("[data-modal-buy-now-mobile-open]"),
+            closeModalBtn: document.querySelector("[data-modal-buy-now-close]"),
+            modal: document.querySelector("[data-modal-buy-now]"),
+            body: document.querySelector("body"),
+        };
+
+        refs.openModalBtn.addEventListener("click", toggleModal);
+        refs.openModalBtnMob.addEventListener("click", toggleModal);
+        refs.closeModalBtn.addEventListener("click", toggleModal);
+
+        function toggleModal() {
+            refs.modal.classList.toggle("is-hidden");
+            refs.body.classList.toggle("no-scroll");
+        }
+    })();
+
+
+// <!-- SCRIPT FOR ENABLED||DISABLED BUTTON -->
+
+
+    const buttonBN = document.querySelector('[data-modal-buy-now-btn]'); 
+    const checkboxBN1 = document.querySelector('[data-modal-buy-now-check-1]'); 
+    const checkboxBN2 = document.querySelector('[data-modal-buy-now-check-2]'); 
+    const checkboxBN3 = document.querySelector('[data-modal-buy-now-check-3]'); 
+    buttonBN.disabled = true; 
+   
+    addEventListener('click', () => { 
+      if (checkboxBN1.checked || checkboxBN2.checked || checkboxBN3.checked) { 
+        buttonBN.disabled = false; 
+      } else { 
+        buttonBN.disabled = true; 
+      } 
+    }); 
